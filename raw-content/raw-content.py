429db3ca21666b0f3bd5242693a9f599689f3c1a
Nziie3 <106406461+Nzii3@users.noreply.github.com>
2022-08-22 18:16:54 -0700

    Update raw-content.py

--- raw-content/raw-content.py
+++ raw-content/raw-content.py
@@ -5,7 +5,14 @@ class Hello(commands.Cog):
       self.bot = bot
 
   @commands.command(help="Get raw content of an embed")
-  async def say(self, ctx, message_id):
+  async def raw(self, ctx, message_id):
+    if not message_id.isnumeric():
+      return await ctx.send("Please provide an integer ID for a message in this channel.")
+    try:
+      msg = await ctx.channel.fetch_message(int(message_id))
+    except:
+      return await ctx.send("That is not a valid message ID in this channel.")
+ 
 
 def setup(bot):
     bot.add_cog(Hello(bot))
