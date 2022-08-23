35e48e70b18d6465564eab1ff6ee03eef8416f9b
Nziie3 <106406461+Nzii3@users.noreply.github.com>
2022-08-22 18:34:22 -0700

    Update raw-content.py

--- raw-content/raw-content.py
+++ raw-content/raw-content.py
@@ -1,10 +1,14 @@
+import discord
 from discord.ext import commands
+from core import checks
 
 class Hello(commands.Cog):
   def __init__(self, bot):
       self.bot = bot
 
-  @commands.command(help="Get raw content of an embed")
+  @commands.group(help="Get raw content of an embed", invoke_without_command=True)
+  @checks.has_permissions(PermissionLevel.SUPPORTER)
+  @checks.thread_only()
   async def raw(self, ctx, message_id):
     if not message_id.isnumeric():
       return await ctx.send("Please provide an integer ID for a message in this channel.")
@@ -15,6 +19,20 @@ class Hello(commands.Cog):
     if len(message.embeds) == 0:
       return await ctx.send("There is no content to make raw! (No embeds).")
     await ctx.send(message.embeds[0].description)
+  
+  @raw.command(name="codeblock", aliases=['cb'], description="Get raw content from an embed (in codeblock)")
+  @checks.has_permissions(PermissionLevel.SUPPORTER)
+  @checks.thread_only()
+  async def raw_codeblock(self, ctx, message_id):
+    if not message_id.isnumeric():
+      return await ctx.send("Please provide an integer ID for a message in this channel.")
+    try:
+      message = await ctx.channel.fetch_message(int(message_id))
+    except:
+      return await ctx.send("That is not a valid message ID in this channel.")
+    if len(message.embeds) == 0:
+      return await ctx.send("There is no content to make raw! (No embeds).")
+    await ctx.send(f"```\n{message.embeds[0].description}\n```")
 
 def setup(bot):
     bot.add_cog(Hello(bot))
