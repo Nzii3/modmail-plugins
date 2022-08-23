1add8e2b3614e4cc3bbd88e24bbf27a4b899a4d2
Nziie3 <106406461+Nzii3@users.noreply.github.com>
2022-08-22 18:50:46 -0700

    Update raw-content.py

--- raw-content/raw-content.py
+++ raw-content/raw-content.py
@@ -6,6 +6,7 @@ from core.models import PermissionLevel
 class Raw(commands.Cog):
   def __init__(self, bot):
       self.bot = bot
+      self.db = bot.api.get_plugin_partition(self)
 
   @commands.group(help="Get raw content of an embed", invoke_without_command=True)
   @checks.has_permissions(PermissionLevel.SUPPORTER)
@@ -32,6 +33,12 @@ class Raw(commands.Cog):
     if len(message.embeds) == 0:
       return await ctx.send("There is no content to make raw! (No embeds).")
     await ctx.send(f"```\n{message.embeds[0].description}\n```")
+
+  @commands.command()
+  @checks.has_permissions(PermissionLevel.OWNER)
+  async def testdb(self, ctx, *, content: str):
+    await self.db.find_one_and_update({'_id': 'config'}, {'$set': {'test_content': content}}, upsert=True)
+    await ctx.send(f"Successfully set `content` as:\n>>> {content}")
 
 def setup(bot):
     bot.add_cog(Raw(bot))
