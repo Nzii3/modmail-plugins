f1abcb2e6c545ccdf595214600dd0d1a836d140b
Nziie3 <106406461+Nzii3@users.noreply.github.com>
2022-10-29 11:08:46 -0700

    .

---
+++ post-creator/README.md
@@ -0,0 +1,8 @@
+# Post Creator
+
+*Post Creator is a plugin specifially for a server, I don't recommend installing this plugin as it won't work for any server that isn't the intended server for it to work in.*
+
+> Created for `William B#4427` on [Discord](https://discord.com)
+
+# Credits
+*Made with 💙 by [vNziie--#7777](https://nziie.is-a.dev)*
---
+++ post-creator/post-creator.py
@@ -0,0 +1,25 @@
+import discord
+from core import checks
+from core.models import PermissionLevel
+from discord.ext import commands
+from discord import app_commands
+from discord.app_commands import Argument
+from .utils import *
+
+class PostFlags(commands.FlagConverter, prefix="?", delimiter=" "):
+  type: str = commands.flag(aliases=['t'], default=None)
+
+class PostCreator(commands.Cog):
+  def __init__(self, bot):
+    self.bot = bot
+    self.version = "1.0.0"
+
+  @app_commands.command(name="forum-post", description="Create a forum post")
+  async def forum_post(self, interaction, title: str, message: str, channel: Argument(discord.AppCommandOptionType.channel, required=False)):
+    await interaction.response.defer(ephemeral=True)
+    channel: discord.ForumChannel = channel or interaction.channel
+    await channel.create_thread(name=title, message=message)
+    await interaction.followup.send(content=f'{self.bot.sent_emoji} Successfully created forum post!', ephemeral=True)
+
+async def setup(bot):
+  await bot.add_cog(PostCreator(bot), guilds=[discord.Object(id=841407843529523200)])
