1787c8b4a85a69f7cfffa85113fb794bf650522e
Nziie3 <106406461+Nzii3@users.noreply.github.com>
2023-03-09 19:31:27 -0800

    Update preview.py

--- preview/preview.py
+++ preview/preview.py
@@ -1,3 +1,19 @@
 # A command to preview threads and thread info
 import discord
 from discord.ext import commands
+from core import checks
+from core.checks import PermissionLevel
+
+class Preview(commands.Cog):
+  def __init__(self, bot):
+    self.bot = bot
+    self.db = self.bot.plugin_db.get_partition(self)
+  
+  @checks.has_permissions(PermissionLevel.SUPPORTER)
+  @commands.command(help="Preview a thread by ID or all")
+  async def preview(self, ctx, thread_id=None):
+    data = await self.db.find_one()
+    await ctx.send(data)
+    
+async def setup(bot):
+  await bot.add_cog(Preview(bot))
