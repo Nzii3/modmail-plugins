310e9114af6637944a69a41b5e8b96414253dbbe
Nziie3 <106406461+Nzii3@users.noreply.github.com>
2023-03-09 19:44:07 -0800

    Update preview.py

--- preview/preview.py
+++ preview/preview.py
@@ -12,7 +12,7 @@ class Preview(commands.Cog):
   @checks.has_permissions(PermissionLevel.SUPPORTER)
   @commands.command(help="Preview a thread by ID or all")
   async def preview(self, ctx, thread_id=None):
-    data = await self.db.find_one({'_id': 'config'})
+    data = await self.db.find_one({'_id': 'threads'})
     print(data)
     await ctx.send(data)
     
